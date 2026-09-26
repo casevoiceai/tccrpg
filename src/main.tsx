74146d366b3b89404bd2147b854fde7201ca25a0
casevoiceai <casevoice.ai@gmail.com>
2026-09-26 03:43:56 -0400

    Add Build 3 privacy route and anonymous-data notice

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, useLocation } from 'react-router-dom'
+import { BrowserRouter, Link, useLocation } from 'react-router-dom'
 import App from './App'
 import { retryPendingPortalSubmission } from './analytics'
 import { PlaytestPage, UpdatesPage } from './Build3Pages'
+import Build3Privacy from './Build3Privacy'
 import './styles.css'
+import './build3.css'
 
 function PortalRoot() {
   const location = useLocation()
@@ -15,8 +17,18 @@ function PortalRoot() {
 
   if (location.pathname === '/playtest') return <PlaytestPage />
   if (location.pathname === '/updates') return <UpdatesPage />
+  if (location.pathname === '/privacy') return <Build3Privacy />
 
-  return <App />
+  return (
+    <>
+      {location.pathname === '/experience' && (
+        <div className="data-notice">
+          Finishing the guided demo sends an anonymous testing snapshot to TCC. No name or email is attached. <Link to="/privacy">What is collected?</Link>
+        </div>
+      )}
+      <App />
+    </>
+  )
 }
 
 createRoot(document.getElementById('root')!).render(
