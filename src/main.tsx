ed9dbc6d6db4ab5724967d98fd9886681c11be51
casevoiceai <casevoice.ai@gmail.com>
2026-09-26 03:42:57 -0400

    Retry pending anonymous Cloudflare submissions on portal load

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App'
+import { retryPendingPortalSubmission } from './analytics'
 import { PlaytestPage, UpdatesPage } from './Build3Pages'
 import './styles.css'
 
 function PortalRoot() {
   const location = useLocation()
+
+  useEffect(() => {
+    retryPendingPortalSubmission()
+  }, [])
 
   if (location.pathname === '/playtest') return <PlaytestPage />
   if (location.pathname === '/updates') return <UpdatesPage />
